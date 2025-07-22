b52f9a3ea33a7c31dfdfb6502bd088cbccaa90de
Kamil Mikurda <kmikurda@abis.krakow.pl>
2025-07-22 15:13:19 +0200

    Create App using PostgreSQL and Redis containers

---
+++ src/ContainerWebApp/Program.cs
@@ -0,0 +1,52 @@
+using Npgsql;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+ 
+
+// Konfiguracja Redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("REDIS_URL") ?? "localhost:6379")
+);
+
+// Konfiguracja PostgreSQL - jako Transient żeby zawsze tworzyć nowe połączenie
+builder.Services.AddTransient<Func<NpgsqlConnection>>(sp => () => 
+    new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE_URL"))
+);
+
+var app = builder.Build();
+//app.Urls.Add("http://0.0.0.0:8080");
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/db", async (Func<NpgsqlConnection> createConnection) =>
+{
+    try
+    {
+        using var dbConn = createConnection();
+        await dbConn.OpenAsync();
+        using var cmd = new NpgsqlCommand("SELECT version()", dbConn);
+        var result = await cmd.ExecuteScalarAsync();
+        return Results.Ok($"PostgreSQL version: {result}");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Database connection error: {ex.Message}");
+    }
+});
+
+app.MapGet("/redis", async (IConnectionMultiplexer redis) =>
+{
+    try
+    {
+        var db = redis.GetDatabase();
+        await db.StringSetAsync("test_key", $"Hello Redis! Time: {DateTime.Now}");
+        var value = await db.StringGetAsync("test_key");
+        return Results.Ok($"Redis Works!: {value}");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Redis connection Error: {ex.Message}");
+    }
+});
+
+app.Run();
